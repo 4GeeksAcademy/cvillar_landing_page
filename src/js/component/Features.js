a9003efd5e9750c2821c1ff63c5107e1fc02c61d
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-07-23 10:15:38 +0000

    [Update] Creacion de los componentes Header,Footer, Feature, Home y sus respectivos css Feature: Creacion de las cartas. Correccion del estilo de la carta por problemas de tamaño (con ayuda de Javier Pestana) Footer: Creacion de un Footer Header: Creacion de Nabvar, y su funcionamiento para que siempre quede arriba cuando se escrolea con celular.

--- src/js/component/Features.js
+++ src/js/component/Features.js
@@ -2,11 +2,33 @@
 import React from 'react';
 import '../../styles/Features.css';
 
+// Importa las imágenes locales
+import tajmajal from '../../img/tajmajal.jpg';
+import stonhenge from '../../img/stonhenge.jpg';
+import murallaImg from '../../img/muralla.jpg';
+import gizaImg from '../../img/giza.jpg';
+
 const featuresData = [
-    { title: 'Feature 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-    { title: 'Feature 2', description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { title: 'Feature 3', description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-    { title: 'Feature 4', description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' }
+    { 
+        title: 'Feature 1', 
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        image: tajmajal
+    },
+    { 
+        title: 'Feature 2', 
+        description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: stonhenge
+    },
+    { 
+        title: 'Feature 3', 
+        description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        image: murallaImg
+    },
+    { 
+        title: 'Feature 4', 
+        description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        image: gizaImg
+    }
 ];
 
 const Features = () => {
@@ -15,8 +37,11 @@ const Features = () => {
             <div className="card-container">
                 {featuresData.map((feature, index) => (
                     <div className="card" key={index}>
-                        <h4>{feature.title}</h4>
-                        <p>{feature.description}</p>
+                        <img src={feature.image} alt={feature.title} className="card-image" />
+                        <div className="card-content">
+                            <h4>{feature.title}</h4>
+                            <p>{feature.description}</p>
+                        </div>
                         <button className="card-button">Find Out More!</button>
                     </div>
                 ))}
